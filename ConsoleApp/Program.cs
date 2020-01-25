cc991861658be7baeb8ae2b98d3e290dc800c937
mariusraht2 <mariusraht+2@gmail.com>
2020-01-25 13:31:29 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static Library.Library;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Book[] books = new Book[3];
+            //Book book1 = new Book("The Never-ending Story", "Michael Ende", BookGenre.Roman, 1979, "9783844912620", 9.99m);
+            //Book book2 = new Book("Pilgrim's Progress", "John Bunyan", BookGenre.Roman, 2007, "9283854912619", 12.99m);
+            //Book book3 = new Book("Robinson Crusoe", "Daniel Defoe", BookGenre.Roman, 2014, "9583841912603", 0.99m);
+            //books[0] = book1;
+            //books[1] = book2;
+            //books[2] = book3;
+
+            //Inventory inventory = new Inventory(books);
+
+            Serializer serializer = new Serializer();
+            Inventory inventory = serializer.ImportFromXml("books.xml");
+            serializer.ExportToXml("books.xml", inventory);
+
+            Console.WriteLine(inventory.ToString());
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Library
+{
+    public class Library
+    {
+        public enum BookGenre
+        {
+            Roman,
+            Sachbuch
+        }
+
+        public class Book
+        {
+            [XmlAttribute]
+            public string Title { get; set; }
+            [XmlAttribute]
+            public string Author { get; set; }
+            public BookGenre Genre { get; set; }
+            public int Year { get; set; }
+            public string ISBN { get; set; }
+            public decimal Price { get; set; }
+
+            public Book() { }
+
+            public Book(string title, string author, BookGenre genre, int year, string isbn, decimal price)
+            {
+                Title = title;
+                Author = author;
+                Genre = genre;
+                Year = year;
+                ISBN = isbn;
+                Price = price;
+            }
+        }
+
+        public class Inventory
+        {
+            public Book[] Books { get; set; }
+
+            private decimal totalValue;
+            public decimal TotalValue
+            {
+                get
+                {
+                    totalValue = 0;
+
+                    for (int i = 0; i < Books.Length; i++)
+                    {
+                        totalValue += Books[i].Price;
+                    }
+
+                    return totalValue;
+                }
+                set
+                {
+                    totalValue = value;
+                }
+            }
+
+            public Inventory() { }
+
+            public Inventory(Book[] books)
+            {
+                Books = books;
+            }
+
+            public override string ToString()
+            {
+                string output = "";
+
+                for (int i = 0; i < Books.Length; i++)
+                {
+                    output += $"{Books[i].Title}\n";
+                }
+                output += $"Gesamtwert: {TotalValue}";
+
+                return output;
+
+            }
+        }
+
+        public class Serializer
+        {
+            public void ExportToXml(string fileName, Inventory inventory)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Inventory));
+
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    serializer.Serialize(writer, inventory);
+                }
+            }
+
+            public Inventory ImportFromXml(string fileName)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Inventory));
+
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    Inventory inventory = (Inventory)serializer.Deserialize(reader);
+                    return inventory;
+                }
+            }
+        }
+    }
+}
